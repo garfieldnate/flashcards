5d262ad473c021c0fd006aaa5808544578e6105f
Nathan Glenn <nathan-gary.glenn@trivago.com>
2019-01-27 20:04:33 +0100

    Add example sentences for Thai card
    Thai has a different font, obviously, so it's good to check how it looks.

--- src/builtinData/cards-dev-thai.js
+++ src/builtinData/cards-dev-thai.js
@@ -4,6 +4,8 @@ const cards = [
         "front": "black",
         "back": "สีดำ",
         "category": "colors",
+        "exampleForeignLang": "ทำไมสีอวกาศจึงมีสีดำ?",
+        "exampleUserLang": "Why is outer space black?",
         "foreignHeadwordAudio": require("../../assets/sounds/decks/dev-thai/headword_foreign_0.mp3"),
     },
 ]
