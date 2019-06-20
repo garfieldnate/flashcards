4422014f83fd7539c4f0fa4e68a7d7ec8bbc5922
Nathan Glenn <nathan-gary.glenn@trivago.com>
2019-06-20 14:32:53 +0200

    Added tslint

--- src/components/AddDeckButton.tsx
+++ src/components/AddDeckButton.tsx
@@ -2,42 +2,47 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 
 import { Button, Icon } from 'native-base';
-import {NavigationScreenProp, NavigationState, NavigationParams} from 'react-navigation'
+import { NavigationScreenProp, NavigationState, NavigationParams } from 'react-navigation';
 
-type Navigation = NavigationScreenProp<NavigationState, NavigationParams>
+type Navigation = NavigationScreenProp<NavigationState, NavigationParams>;
 
 type Props = {
   iconStyle: Icon['props']['style'],
-  navigation: Navigation
-}
+  navigation: Navigation,
+};
 
 export default class AddDeckButton extends Component<Props, {}> {
   render() {
     return (
-      <Button transparent
-            style={styles.button}
-            onPress={this.handleClicked}>
+      <Button
+        transparent
+        style={styles.button}
+        onPress={this.handleClicked}
+      >
         <Icon
-            style={this.props.iconStyle}
-            name="ios-add-circle-outline" />
-    </Button>);
+          style={this.props.iconStyle}
+          name='ios-add-circle-outline'
+        />
+      </Button>
+    );
   }
 
   handleClicked = () => {
     const navigation = this.props.navigation;
-    navigation.navigate('AddDecks',
+    navigation.navigate(
+      'AddDecks',
       {
-        userData:   navigation.state.params.userData,
-        deckSource: navigation.state.params.deckSource
+        userData: navigation.state.params.userData,
+        deckSource: navigation.state.params.deckSource,
       });
   }
 }
 
 const styles = StyleSheet.create({
-    button: {
-        alignSelf: "center",
-        // without this, the bottom of the icon can get cut off
-        paddingTop: 0,
-        paddingBottom: 0
-    }
+  button: {
+    alignSelf: 'center',
+    // without this, the bottom of the icon can get cut off
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
 });
