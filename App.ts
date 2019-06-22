1a52d1917047be4b98cf912e19bc5d3c2acc9abd
Nathan Glenn <nathan-gary.glenn@trivago.com>
2019-06-22 22:44:23 +0200

    package.json cleanup
    Jest config has its own file, so we can remove it from package.json.
    
    Change the lint command so that it is also run on App.ts.

--- App.ts
+++ App.ts
@@ -1,34 +1,33 @@
-import React from 'react';
-import StudyScreen from './src/screens/StudyScreen';
-import ChooseStudyDeckScreen from './src/screens/ChooseStudyDeckScreen';
-import AddDecksScreen from './src/screens/AddDecksScreen';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+import AddDecksScreen from './src/screens/AddDecksScreen';
+import ChooseStudyDeckScreen from './src/screens/ChooseStudyDeckScreen';
+import StudyScreen from './src/screens/StudyScreen';
 
-import DummyUserData from './src/userData/DummyUserData';
 import DummyDeckSource from './src/builtinData/DummyDeckSource';
+import DummyUserData from './src/userData/DummyUserData';
 
 const userData = new DummyUserData();
 const deckSource = new DummyDeckSource();
 
 const AppNavigator = createStackNavigator(
   {
-    ChooseStudyDeck: ChooseStudyDeckScreen,
     AddDecks: AddDecksScreen,
+    ChooseStudyDeck: ChooseStudyDeckScreen,
     Study: StudyScreen,
   },
   {
-    initialRouteName: 'ChooseStudyDeck',
-    initialRouteParams: { userData, deckSource },
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: 'rgba(26, 84, 147, 1)',
       },
+      // colors the back button and text
+      headerTintColor: '#D1DCE9',
       headerTitleStyle: {
         color: '#D1DCE9',
       },
-      // colors the back button and text
-      headerTintColor: '#D1DCE9',
     },
+    initialRouteName: 'ChooseStudyDeck',
+    initialRouteParams: { userData, deckSource },
   }
 );
 
